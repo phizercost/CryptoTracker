9577edce54c587f7d7520fa259010f409c6a4aef
phizercost <phizer.development@gmail.com>
2019-01-15 09:32:11 +0200

    Finalized RootViewer with no storyboard and Coin main class

---
+++ Crypto Tracker/CoinData.swift
@@ -0,0 +1,17 @@
+//
+//  CoinData.swift
+//  Crypto Tracker
+//
+//  Created by Phizer Cost on 1/14/19.
+//  Copyright © 2019 Phizer Cost. All rights reserved.
+//
+
+import UIKit
+
+class Coin {
+    var symbol = ""
+    var image = UIImage()
+    var price = 0.0
+    var amount = 0.0
+    var historicalData = [Double]()
+}
